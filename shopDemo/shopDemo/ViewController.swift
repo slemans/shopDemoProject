e913545b374d20ea6b1b4680026633489c8dd6f0
Pavel Bazylevich <sleman@yandex.ru>
2023-03-16 12:31:26 +0300

    Create project

---
+++ shopDemo/shopDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shopDemo
+//
+//  Created by Павел on 16.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
